b59052fa542b7adcf75325d7c3f29cf5738aee02
jaLeague <jackie.arseneau@jointheleague.org>
2019-03-05 15:53:36 -0800

    Add files via upload

---
+++ src/SimonSaysRunner.java
@@ -0,0 +1,11 @@
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+public class SimonSaysRunner {
+	public static void main(String[] args){
+		new SimonSays().run();
+	}
+
+}
