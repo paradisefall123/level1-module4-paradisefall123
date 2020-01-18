1cd4fd06a48b6a5911dadda50a8b1b846037dfd0
League <league@Admins-Mac-mini.local>
2020-01-18 11:31:02 -0800

    i need to finish book of illusions

--- src/_06_book_of_illusions/BookOfIllusions.java
+++ src/_06_book_of_illusions/BookOfIllusions.java
@@ -17,6 +17,7 @@ public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
 		JFrame frame=new JFrame();
+	JLabel label;
 	public void run() {
 		// 2. make the frame visible
 		frame.setVisible(true);
@@ -24,18 +25,33 @@ public class BookOfIllusions extends MouseAdapter {
 		frame.setSize(500,500);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+        String image="colorful.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+        label=loadImageFromComputer(image);
+
 		// 8. add your JLabel to the frame
+        frame.add(label);
+
 		// 9. call the pack() method on the frame
+        frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+        frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+        System.out.println("clicked!");
 		// 12. remove everything from the frame that was added earlier
+
 		// 13. load a new image like before (this is more than one line of code)
+
+		String twoimage="tehehe.jpg";
+		label=loadImageFromComputer(twoimage);
+		frame.add(label);
 		// 14. pack the frame
+		frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
