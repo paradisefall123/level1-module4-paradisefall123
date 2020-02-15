ebe7fb1b203388bf694fad74fa2cac4d0ee67770
paradisefall123 <paradisewendy123@gmail.com>
2020-02-15 10:38:39 -0800

    -Working on calculator

--- src/_08_calculator/CalculatorRunner.java
+++ src/_08_calculator/CalculatorRunner.java
@@ -1,5 +1,14 @@
 package _08_calculator;
 
-public class CalculatorRunner {
-    public static void main(String[] args) {new Calculator().run;}
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class  CalculatorRunner implements ActionListener {
+    public static void main(String[] args) {new Calculator().run();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        
+    }
 }
