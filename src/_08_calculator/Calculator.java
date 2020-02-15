6bd4a3cadee82351f5240f0253a5391abd94dbdb
paradisefall123 <paradisewendy123@gmail.com>
2020-02-15 11:00:02 -0800

    good morning

--- src/_08_calculator/Calculator.java
+++ src/_08_calculator/Calculator.java
@@ -1,14 +1,22 @@
 package _08_calculator;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Calculator {
+public class Calculator implements ActionListener {
     public void run(){
         JFrame frame=new JFrame();
         JPanel panel=new JPanel();
         JButton addition=new JButton();
-
+        JButton subtraction = new JButton();
         frame.setVisible(true);
+        frame.setTitle("Simple Calculator");
+        panel.add(addition);
+        addition.setText("Add");
+        //addition.addActionListener(this);
+        frame.add(panel);
+        frame.pack();
     }
     public void add(){
 
@@ -23,4 +31,8 @@ public class Calculator {
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- src/_08_calculator/CalculatorRunner.java
+++ src/_08_calculator/CalculatorRunner.java
@@ -1,14 +1,7 @@
 package _08_calculator;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class  CalculatorRunner implements ActionListener {
-    public static void main(String[] args) {new Calculator().run();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        
+public class CalculatorRunner {
+    public static void main(String[] args) {
+        new Calculator().run();
     }
 }
