2e6350fa545f6e0a1aa6982322713e235f13ed5e
wendygonzalez <paradisewendy123@gmail.com>
2020-05-09 11:26:58 -0700

    I need to finish whack a mole.

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -12,6 +12,7 @@ public class WhackAMole implements ActionListener {
     private String s;
     private int num;
     private JFrame frame;
+
     public void run() {
         frame = new JFrame();
         panel = new JPanel();
@@ -19,24 +20,31 @@ public class WhackAMole implements ActionListener {
         frame.setTitle("Whack a Button for Fame and Glory");
         frame.setSize(250, 300);
         Random ran = new Random();
-        num= ran.nextInt(24);
+        num = ran.nextInt(25);
         frame.setVisible(true);
         drawButtons(num);
         frame.validate();
     }
 
-    public void drawButtons( int num) {
+    public void drawButtons(int num) {
         for (int i = 1; i < 25; i++) {
-            s=Integer.toString(i);
-            JButton button = new JButton(s);
+            JButton button;
+            s = Integer.toString(i);
+            if (i == num) {
+                button = new JButton("mole!");
+
+            } else {
+                button = new JButton(s);
+            }
             button.addActionListener(this);
-            System.out.println("draw button; "+i);
+            System.out.println("draw button; " + i);
 
-           button.setActionCommand(s);
+            button.setActionCommand(s);
             panel.add(button);
-        }
 
+        }
     }
+
     void speak(String words) {
         try {
             Runtime.getRuntime().exec("say " + words).waitFor();
@@ -44,13 +52,20 @@ public class WhackAMole implements ActionListener {
             e.printStackTrace();
         }
     }
-
+    private void playSound(String fileName) {
+        AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+        sound.play();
+    }
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         System.out.println("Pressed!");
-    int p=Integer.parseInt(s);
-   //if(){
+        int p = Integer.parseInt(s);
+        if(p!=num){
+        speak("Wrong");
+        frame.dispose();
+        run();
+        }else if(p==num ){
 
-    //}
+        }
     }
 }
