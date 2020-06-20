818dfc38e2d5e7d2eaed31bad089c303d8a4b3f5
wendygonzalez <paradisewendy123@gmail.com>
2020-06-20 11:23:12 -0700

    I need to finish slot machine.

--- src/_11_whack_a_mole/SlotMachine.java
+++ src/_11_whack_a_mole/SlotMachine.java
@@ -1,23 +1,36 @@
 package _11_whack_a_mole;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class SlotMachine {
-    public void run(){
-    JFrame frame=new JFrame();
-    JPanel panel=new JPanel();
-    frame.setVisible(true);
-
-    frame.add(panel);
-    JButton spinButton=new JButton();
-    panel.add(spinButton);
-    spinButton.setText("SPIN");
+public class SlotMachine implements ActionListener {
+    JLabel fruit;
+    public void run() throws MalformedURLException {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.setVisible(true);
+        frame.setSize(500, 500);
+        frame.add(panel);
+        JButton spinButton = new JButton();
+        panel.add(spinButton);
+        spinButton.setText("SPIN");
+        spinButton.addActionListener(this);
+        String cherry="cherry.jpg";
+        String leaf="leaf.jpeg";
+        String seven="seven.jpeg";
+        fruit=createLabelImage(cherry);
+        panel.add(fruit);
+        //createLabelImage("cherry");
+        //createLabelImage("leaf.jpeg");
+        //createLabelImage("seven.jpeg");
     }
+
     private JLabel createLabelImage(String fileName) throws MalformedURLException {
         URL imageURL = getClass().getResource(fileName);
-        if (imageURL == null){
+        if (imageURL == null) {
             System.err.println("Could not find image " + fileName);
             return new JLabel();
         }
@@ -26,4 +39,8 @@ public class SlotMachine {
         return imageLabel;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
 }
--- src/_11_whack_a_mole/SlotMachineRunner.java
+++ src/_11_whack_a_mole/SlotMachineRunner.java
@@ -1,5 +1,7 @@
 package _11_whack_a_mole;
 
+import java.net.MalformedURLException;
+
 public class SlotMachineRunner {
-    public static void main(String[] args){ new SlotMachine().run();}
+    public static void main(String[] args) throws MalformedURLException { new SlotMachine().run();}
 }
