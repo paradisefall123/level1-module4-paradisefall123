d44d03183335cc10c5535e037261594bc64defd7
wendygonzalez <paradisewendy123@gmail.com>
2021-01-08 22:31:14 -0800

    Practice test finished.

---
+++ src/PracticeTest.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PracticeTest implements ActionListener {
+    JFrame frame;
+    JLabel label;
+    JButton submit;
+    JButton hint;
+    JTextField answer;
+    JPanel panel;
+
+    void run() {
+        frame = new JFrame();
+        panel = new JPanel();
+        label = new JLabel();
+        submit = new JButton();
+        hint = new JButton();
+        answer = new JTextField(14);
+        frame.setVisible(true);
+        frame.setTitle("Riddle");
+        frame.add(panel);
+        panel.add(label);
+        panel.add(answer);
+        panel.add(submit);
+        panel.add(hint);
+        answer.addActionListener(this);
+        submit.addActionListener(this);
+        hint.addActionListener(this);
+        submit.setSize(70, 70);
+        hint.setSize(70, 70);
+        label.setText("What belongs to you, but others use it more than you?");
+        submit.setText("Submit");
+        hint.setText("Hint");
+        frame.pack();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        JButton buttonClicked = (JButton) actionEvent.getSource();
+        String response = answer.getText();
+        if (actionEvent.getActionCommand().equals("Hint")) {
+            JOptionPane.showMessageDialog(null, "It was given to you at birth by your parents.");
+        } else if (response.equalsIgnoreCase("Your name")) {
+            JOptionPane.showMessageDialog(null, "Correct");
+        } else {
+            JOptionPane.showMessageDialog(null, "Incorrect");
+        }
+
+    }
+
+}
---
+++ src/PracticeTestRunner.java
@@ -0,0 +1,6 @@
+public class PracticeTestRunner {
+    public static void main(String[] args) {
+        new PracticeTest().run();
+
+    }
+}
